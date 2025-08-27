ff9092cf760aa29cb2a73e2f3c0ab87594957ff6
Pramod <pramodkumar.damam73@gmail.com>
2025-08-27 21:30:59 +0530

    Country name

--- front/app/message/country-flag.tsx
+++ front/app/message/country-flag.tsx
@@ -1,20 +1,24 @@
 import type { Location } from "libs/prisma";
+import { useMemo } from "react";
+import { getCountryData, type TCountryCode } from "countries-list";
 
 export function CountryFlag({ location }: { location: Location }) {
-  if (!location.country) {
+  const country = useMemo(
+    () => location.country && getCountryData(location.country as TCountryCode),
+    [location.country]
+  );
+  if (!country || !location.country) {
     return null;
   }
 
   return (
     <div
       className="tooltip"
-      data-tip={[location.city, location.region, location.country]
-        .filter(Boolean)
-        .join(", ")}
+      data-tip={[location.city, country.name].filter(Boolean).join(", ")}
     >
       <img
         src={`https://purecatamphetamine.github.io/country-flag-icons/3x2/${location.country.toUpperCase()}.svg`}
-        alt={location.country}
+        alt={country.name}
         className="min-w-8 rounded-box"
       />
     </div>
